4272455d23a67be06f2b26b0ce9448b5575114bf
daneography <acena.dane@gmail.com>
2017-06-27 13:40:22 -0400

    version 0.4
    Initial only License agreement... ability to initiate tutorial again...

--- app/src/main/java/uofsc/com/breatheasynologin/welcomeActivity.java
+++ app/src/main/java/uofsc/com/breatheasynologin/welcomeActivity.java
@@ -36,12 +36,12 @@ public class welcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Checking for first time launch - before calling setContentView()
+        /*// Checking for first time launch - before calling setContentView()
         prefManager = new PrefManager(this);
         if (!prefManager.isFirstTimeLaunch()) {
             launchHomeScreen();
             finish();
-        }
+        }*/
 
         // Making notification bar transparent
         if (Build.VERSION.SDK_INT >= 21) {
@@ -121,7 +121,6 @@ public class welcomeActivity extends AppCompatActivity {
     }
 
     private void launchHomeScreen() {
-        prefManager.setFirstTimeLaunch(false);
         startActivity(new Intent(welcomeActivity.this, MainMenuNavigation.class));
         finish();
     }
